bb434534fe8451e159f07f418eb54b93847f06c2
Agus Awaludin <aogust252000@gmail.com>
2021-06-05 11:08:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if2_10118067_agusawaludin/BerhasilSimpan.java
@@ -0,0 +1,21 @@
+package com.example.utspraktik_if2_10118067_agusawaludin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+Tanggal Pengerjaan  : 5 Juni 2021
+Nim                 : 10118067
+Nama                : Agus Awaludin
+Kelas               : IF-2
+
+ */
+
+public class BerhasilSimpan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berhasil_simpan);
+    }
+}
---
+++ app/src/main/java/com/example/utspraktik_if2_10118067_agusawaludin/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.utspraktik_if2_10118067_agusawaludin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+Tanggal Pengerjaan  : 5 Juni 2021
+Nim                 : 10118067
+Nama                : Agus Awaludin
+Kelas               : IF-2
+
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
